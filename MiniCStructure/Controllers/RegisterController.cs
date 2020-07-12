945b891fa799c0fb68c3af9131c22e29461aaf06
Michael Bowen <mikee.bowen@gmail.com>
2020-07-12 14:32:29 -0700

    add register view and post endpoint

---
+++ MiniCStructure/Controllers/RegisterController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiniCStructure.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string email, string password, string passwordConfirm)
+        {
+            return Content($"{email}, {password}, {passwordConfirm}");
+        }
+    }
+}
